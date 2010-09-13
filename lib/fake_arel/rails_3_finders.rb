d49d43f5dfd71ac9cd859272eaee13b9351aeb00
Sokolov Yura <funny.falcon@gmail.com>
2010-09-13 20:25:05 +0400

    fix stupid error

--- lib/fake_arel/rails_3_finders.rb
+++ lib/fake_arel/rails_3_finders.rb
@@ -9,7 +9,7 @@ module Rails3Finders
       named_scope :includes, lambda { |*includes| { :include => includes }}
       named_scope :select, lambda {|*select| {:select => select.join(',') }}
       named_scope :order, lambda {|*order| {:order => order.join(',') }}
-      named_scope :joins, lambda {|*join| {:joins => join } if joins[0]}
+      named_scope :joins, lambda {|*join| {:joins => join } if join[0]}
       named_scope :from, lambda {|*from| {:from => from }}
       named_scope :having, lambda {|*having| {:having => having }}
       named_scope :group, lambda {|*group| {:group => group }}
