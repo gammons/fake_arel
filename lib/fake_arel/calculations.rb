f14aeb16278b75c7c92e196a9dc5640ad71bbfab
Sokolov Yura <funny.falcon@gmail.com>
2010-12-07 12:55:09 +0300

    merge allready creates copy

--- lib/fake_arel/calculations.rb
+++ lib/fake_arel/calculations.rb
@@ -4,7 +4,7 @@ module ActiveRecord
       #fix calculations to consider scoped :group
       def calculate_with_fakearel(operation, column_name, options = {})
         if !options[:group] && (cur_scope = scope(:find)) && cur_scope[:group]
-          options = options.dup.merge(:group => cur_scope[:group])
+          options = options.merge(:group => cur_scope[:group])
         end
         calculate_without_fakearel(operation, column_name, options)
       end
