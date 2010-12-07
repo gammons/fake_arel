f8cb03e15de2bf24d1db9e2d55b0e8580f8dc0e0
Sokolov Yura <funny.falcon@gmail.com>
2010-12-07 13:04:45 +0300

    consider having also

--- lib/fake_arel/calculations.rb
+++ lib/fake_arel/calculations.rb
@@ -3,8 +3,9 @@ module ActiveRecord
     module ClassMethods
       #fix calculations to consider scoped :group
       def calculate_with_fakearel(operation, column_name, options = {})
-        if !options[:group] && (cur_scope = scope(:find)) && cur_scope[:group]
-          options = options.merge(:group => cur_scope[:group])
+        cur_scope = scope(:find)
+        if !options[:group] && cur_scope && cur_scope[:group]
+          options = options.merge(cur_scope.slice(:group, :having))
         end
         calculate_without_fakearel(operation, column_name, options)
       end
