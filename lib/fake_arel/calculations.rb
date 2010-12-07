0d9e82e54bec8fd73f5f582ddcd629efc99ebedf
Sokolov Yura <funny.falcon@gmail.com>
2010-12-07 13:09:24 +0300

    reverse_merge is more adequate (could save :having)

--- lib/fake_arel/calculations.rb
+++ lib/fake_arel/calculations.rb
@@ -5,7 +5,7 @@ module ActiveRecord
       def calculate_with_fakearel(operation, column_name, options = {})
         cur_scope = scope(:find)
         if !options[:group] && cur_scope && cur_scope[:group]
-          options = options.merge(cur_scope.slice(:group, :having))
+          options = options.reverse_merge(cur_scope.slice(:group, :having))
         end
         calculate_without_fakearel(operation, column_name, options)
       end
